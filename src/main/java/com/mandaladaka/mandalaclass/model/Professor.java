c28fa185e5d428ff715c3192ed6a3b87c8cd97fc
Sgnsilv <sara.silva.114@ufrn.edu.br>
2026-01-12 16:53:09 -0300

    refactor: initial project commit

---
+++ src/main/java/com/mandaladaka/mandalaclass/model/Professor.java
@@ -0,0 +1,23 @@
+package com.mandaladaka.mandalaclass.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "professores")
+
+public class Professor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String senha;
+}
