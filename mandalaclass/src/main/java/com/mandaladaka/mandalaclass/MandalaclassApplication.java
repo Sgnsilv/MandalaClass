0ca56137297c1ae43addc84d0f09532afcfe3ed4
Sgnsilv <sara.silva.114@ufrn.edu.br>
2026-01-12 16:30:09 -0300

    chore: initial project setup

---
+++ mandalaclass/src/main/java/com/mandaladaka/mandalaclass/MandalaclassApplication.java
@@ -0,0 +1,13 @@
+package com.mandaladaka.mandalaclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MandalaclassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MandalaclassApplication.class, args);
+	}
+
+}
---
+++ mandalaclass/src/test/java/com/mandaladaka/mandalaclass/MandalaclassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mandaladaka.mandalaclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MandalaclassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
